96ef2e3df50bf01ab01eb17526fe84577b244b6d
Santiago Caci <santiago.caci@gmail.com>
2022-03-28 16:34:49 -0300

    126 Completado

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import { searchHero } from './js/callbacks';
+import { searchHero as buscasrHeroesCallback } from './js/callbacks';
+import { buscarHeroes } from './js/promesas';
 import './styles.css';
 
-const heroId1 = 'capi';
-const heroId2 = 'spider';
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
 
-searchHero(heroId1, (err, heroe1) => {
+// searchHero(heroId1, (err, heroe1) => {
 
-  if (err) { return console.error(err); };
+//   if (err) { return console.error(err); };
 
-  searchHero(heroId2, (err, heroe2) => {
-    if (err) { return console.error(err); };
-    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
-  })
-});
+//   searchHero(heroId2, (err, heroe2) => {
+//     if (err) { return console.error(err); };
+//     console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
+//   })
+// });
+
+buscarHeroes( heroeId1 ).then( heroe => {
+  console.log(`Enviando a ${heroe.nombre} a la mision`);
+}); 
---
+++ src/js/promesas.js
@@ -0,0 +1,27 @@
+const heroes = {
+  capi: {
+    nombre: 'Capitan America',
+    poder: 'Aguantar inyecciones sin morir',
+  },
+  iron: {
+    nombre: 'Ironman',
+    poder: 'Absurda cantidad de dinero',
+  },
+  spider: {
+    nombre: 'Spiderman',
+    poder: 'La mejor reaccion alergica a las picaduras de arañas',
+  },
+};
+
+export const buscarHeroes = (id) => {
+
+  const heroe = heroes[id];
+
+  return new Promise( (resolve, reject)=> {
+    if(heroe){
+      resolve(heroe);
+    }else{
+      reject(`No existe un hero con el id ${id}`);
+    }
+  });
+}
