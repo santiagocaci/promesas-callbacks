724f04f507a1aa59287df115bbb3502c7f6f13c3
Santiago Caci <santiago.caci@gmail.com>
2022-03-28 16:46:54 -0300

    127 Completado

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ const heroeId2 = 'spider';
 //   })
 // });
 
-buscarHeroes( heroeId1 ).then( heroe => {
-  console.log(`Enviando a ${heroe.nombre} a la mision`);
-}); 
+// buscarHeroes( heroeId1 ).then( heroe1 => {
+//   // console.log(`Enviando a ${heroe.nombre} a la mision`);
+//   buscarHeroes(heroeId2).then(heroe2 => {
+//     console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
+//   })
+// });
+
+
+Promise.all([buscarHeroes(heroeId1), buscarHeroes(heroeId2)])
+  .then(([heroe1, heroe2]) => {
+    console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre}`);
+  });
