d54ca11395feeced161248bf05b45088261997d1
Santiago Caci <santiago.caci@gmail.com>
2022-03-28 17:07:56 -0300

    129 Completado

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { promesaLenta, promesaMedia, promesaRapida } from "./js/promesas";
+
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+Promise.race([promesaLenta, promesaMedia, promesaRapida]).then(console
+  .log);
--- src/js/promesas.js
+++ src/js/promesas.js
@@ -25,3 +25,27 @@ export const buscarHeroes = (id) => {
     }
   });
 }
+
+const promesaLenta = new Promise((resolve, reject)=>{
+  setTimeout(() => {
+    resolve('Promesa Lenta');
+  }, 2000);
+});
+
+const promesaMedia = new Promise((resolve, reject)=>{
+  setTimeout(() => {
+    resolve('Promesa Media');
+  }, 1500);
+});
+
+const promesaRapida = new Promise((resolve, reject)=>{
+  setTimeout(() => {
+    resolve('Promesa Rapida');
+  }, 1000);
+});
+
+export {
+  promesaLenta,
+  promesaMedia,
+  promesaRapida,
+}
