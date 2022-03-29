85c90ef0a35a3a516da7afd841798d9951603040
Santiago Caci <santiago.caci@gmail.com>
2022-03-29 01:11:24 -0300

    Final Seccion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { obtenerHeroesArr, obtenerHeroeAwait } from "./js/await";
+import { heroesCiclo, heroeIfAwait } from "./js/await";
 
-console.time('await');
+heroesCiclo();
 
-obtenerHeroeAwait('capi2')
-  .then(heroe => {
-    console.table(heroe);
-    console.timeEnd('await');
-  });
+heroeIfAwait('iron');
--- src/js/await.js
+++ src/js/await.js
@@ -1,10 +1,11 @@
 import { buscarHeroesAsync, buscarHeroes } from "./promesas";
 
 const heroesIds = ['capi', 'iron', 'spider'];
+const heroesPromesas = heroesIds.map(buscarHeroes);
 
 export const obtenerHeroesArr = async () => {
 
-  return await Promise.all(heroesIds.map( buscarHeroes ));
+  return await Promise.all(heroesIds.map(buscarHeroes));
   // const heroesArr = [];
 
   // for (const id of heroesIds) {
@@ -16,16 +17,35 @@ export const obtenerHeroesArr = async () => {
 };
 
 export const obtenerHeroeAwait = async (id) => {
-
   try {
     const heroe = await buscarHeroesAsync(id);
     return heroe;
   } catch (error) {
-    console.log('catch'); 
+    console.log('catch');
 
-    return{
+    return {
       nombre: 'siname',
       poder: 'sinpoder'
     }
   }
 };
+
+export const heroesCiclo = async () => {
+  console.time('H');
+
+  for await (const heroe of heroesPromesas) {
+    console.log(heroe);
+  }
+  // const heroes = await Promise.all(heroesPromesas);
+  // heroes.forEach(e => console.log(e));
+
+  console.timeEnd('H');
+}
+
+export const heroeIfAwait = async (id) => {
+  if ((await buscarHeroesAsync(id)).nombre === 'Ironman') {
+    console.log('Ironman');
+  } else {
+    console.log('No lo es');
+  }
+}
